c12f7415dc344be5c6e38ba54162c55f14c11409
Eliam <Texsword76@gmail.com>
2025-06-23 10:46:54 +0200

    validate day in Date constructor and account for leap years

--- ex00/src/Date.cpp
+++ ex00/src/Date.cpp
@@ -46,11 +46,21 @@ int Date::getDay() const {
 
 // Constructor for year, month, and day
 Date::Date(const std::string &date) {
+
+	 int date_month[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
 	size_t pos1 = date.find('-');
 	size_t pos2 = date.find('-', pos1 + 1);
 	_year = std::stoi(date.substr(0, pos1));
 	_month = std::stoi(date.substr(pos1 + 1, pos2 - pos1 - 1));
 	_day = std::stoi(date.substr(pos2 + 1));
+
+	if ((_year % 4 == 0 && _year % 100 != 0) || (_year % 400 == 0)) {
+		date_month[1] = 29;
+	}
+
+	if (_day > date_month[_month - 1])
+		_day = 99;
 }
 
 bool Date::operator<(const Date &other) const {
